304c5267611ccf1a1cdd766850df5ae6130f3a2f
Jose RF Junior <web2ajax@gmail.com>
2020-07-13 21:53:03 -0400

    imagens

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   /*CLIENTE FISICA   φ = '1,61803398874988'; */
   clienteFlisicaURLInsert: 'http://191.252.204.57/api/v1/clientefisica/todos',
-  clienteFlisicaURLListarTodos: 'http://localhost:8080/api/v1/clientefisica/todos',
-  clienteFlisicaURLpainel: 'http://177.47.18.202/zuul//brokerages/brokerages/painel',
+  clienteFlisicaURLListarTodos: 'http://localhost/api/v1/clientefisica/todos',
+  clienteFlisicaURLpainel: 'http://191.252.204.57/api/v1/clientefisica/painel',
   clienteFlisicaURLfindName: 'http://177.47.18.202/zuul/brokerages/brokerages/findbyname/',
   clienteFlisicaURLfindId: 'http://177.47.18.202/zuul/brokerages/brokerages/findbyid',
   clienteFlisicaURLfindAccount: 'http://177.47.18.202/zuul/brokerages/brokerages/findaccountsbyuser/',
